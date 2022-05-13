6bce34bc93323ca315cab8feed6000633fe542be
simofilipp <81241738+simofilipp@users.noreply.github.com>
2022-05-13 11:36:37 +0200

    Animazione iniziale da bottone

--- Progetto_Stage/Assets/Simone/Scripts/StartSequenceManager.cs
+++ Progetto_Stage/Assets/Simone/Scripts/StartSequenceManager.cs
@@ -6,9 +6,12 @@ public class StartSequenceManager : MonoBehaviour
 {
     [SerializeField] GameObject cassetto;
     [SerializeField] GameObject gabbia;
+    [SerializeField] float dissolveTimeGabbia;
+    [SerializeField] float dissolveTimeSfere;
     [SerializeField] GameObject ponte;
     [SerializeField] GameObject tastiera;
     [SerializeField] List<GameObject> sferette;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,29 @@ public class StartSequenceManager : MonoBehaviour
 
     public void StartFreeMode()
     {
+        ponte.SetActive(false);
+        var solve = gabbia.GetComponent<MeshRenderer>().material;
+        LeanTween.value(-0.2f, 1f, dissolveTimeGabbia).setOnUpdate((float value) =>
+        {
+            solve.SetFloat("_Dissolvenza_animazione", value);
+        });
+        //.setOnComplete(() => 
+        //{ 
+        //    //attivare tastiera
+
+        //    //animazione terra che sale
+
+        //    //Animazione entrata palline con dissolve
+        //    foreach(var obj in sferette)
+        //    {
+        //        obj.SetActive(true);
+        //        var solveSfera = obj.GetComponent<MeshRenderer>().material;
+        //        LeanTween.value(-1f, 1f, dissolveTimeSfere).setOnUpdate((float value) =>
+        //        {
+        //            solve.SetFloat("_Dissolvenza_animazione", value);
+        //        });
+        //    }
+        //});
 
     }
 }
