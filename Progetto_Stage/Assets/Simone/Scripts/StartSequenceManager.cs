7ecfdd9f33d0d76033eb1ece01e9b3282aa66963
DavideManfredini <81241712+DavideManfredini@users.noreply.github.com>
2022-05-20 16:30:40 +0200

    Nuovo_ambiente
    Animazione ai nuovi modelli, testures e fade

--- Progetto_Stage/Assets/Simone/Scripts/StartSequenceManager.cs
+++ Progetto_Stage/Assets/Simone/Scripts/StartSequenceManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class StartSequenceManager : MonoBehaviour
 {
     [SerializeField] GameObject cassetto;
-    [SerializeField] GameObject gabbia;
+    [SerializeField] GameObject pannelli;
     [SerializeField] float dissolveTimeGabbia;
     [SerializeField] float dissolveTimeSfere;
     [SerializeField] GameObject ponte;
@@ -24,35 +24,42 @@ public class StartSequenceManager : MonoBehaviour
     public void StartFreeMode()
     {
         ponte.SetActive(false);
-        var solve = gabbia.GetComponent<MeshRenderer>().material;
+        var solve = pannelli.GetComponent<MeshRenderer>().materials;
         LeanTween.value(-0.2f, 1f, dissolveTimeGabbia).setOnUpdate((float value) =>
         {
-            solve.SetFloat("_Dissolvenza_animazione", value);
+            solve[1].SetFloat("_Dissolvenza_animazione", value);
         }).setOnComplete(() =>
         {
+            LeanTween.value(-0.2f, 1f, dissolveTimeGabbia).setOnUpdate((float value) =>
+            {
+                solve[0].SetFloat("_Dissolvenza_animazione", value);
+            }).setOnComplete(() =>
+            {
             //attivare tastiera e canvas
             tastiera.SetActive(true);
-            canvasFinale.SetActive(true);
+                canvasFinale.SetActive(true);
             //animazione terra che si ingrandisce, luna che scompare
-            terra.transform.LeanScale(new Vector3(10, 10, 10), 5f).setEaseInOutQuart().setOnComplete(() => { luna.SetActive(false); }).delay=0.5f;
-            
+            terra.transform.LeanScale(new Vector3(10, 10, 10), 5f).setEaseInOutQuart().setOnComplete(() => { luna.SetActive(false); }).delay = 0.5f;
+
 
             //Animazione entrata palline con dissolve
             foreach (var obj in sferette)
-            {
-                obj.SetActive(true);
-                
-                var solveSfera = obj.GetComponentsInChildren<MeshRenderer>()[0].material;
-                LeanTween.value(1f, -0.2f, dissolveTimeSfere).setOnUpdate((float value) =>
                 {
-                    solveSfera.SetFloat("_Dissolvenza_animazione", value);
-                });
-            }
+                    obj.SetActive(true);
 
-            freccia.SetActive(true);
-            freccia1.SetActive(true);
-            freccia2.SetActive(true);
-        });
+                    var solveSfera = obj.GetComponentsInChildren<MeshRenderer>()[0].material;
+                    LeanTween.value(1f, -0.2f, dissolveTimeSfere).setOnUpdate((float value) =>
+                        {
+                            solveSfera.SetFloat("_Dissolvenza_animazione", value);
+                        });
+                }
+
+                freccia.SetActive(true);
+                freccia1.SetActive(true);
+                freccia2.SetActive(true);
+            });
+        }
+            );
 
     }
 }
