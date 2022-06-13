deb2e2bebdc01561d500cafcd201ab3798904e60
simofilipp <81241738+simofilipp@users.noreply.github.com>
2022-06-13 17:53:01 +0200

    Dissolve tablet e tavolo

--- Progetto_Stage/Assets/Simone/Scripts/InitialTriggerManager.cs
+++ Progetto_Stage/Assets/Simone/Scripts/InitialTriggerManager.cs
@@ -9,6 +9,8 @@ public class InitialTriggerManager : MonoBehaviour
     [SerializeField] Transform moon;
     [SerializeField] GameObject console;
     [SerializeField] GameObject tavolo;
+    [SerializeField] List<Material> tavoloMats;
+    [SerializeField] List<Material> matSpecials;
     [SerializeField] GameObject tablet;
     [SerializeField] Transform padre;
     public SoundFXRef testSound1;
@@ -16,6 +18,17 @@ public class InitialTriggerManager : MonoBehaviour
     Vector3 earthScale;
     Vector3 moonScale;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        foreach (var renderer in tavoloMats)
+        {
+                renderer.SetFloat("_Dissolvenza_animazione", 1);
+        }
+        foreach (var renderer in matSpecials)
+        {
+            renderer.SetFloat("_Dissolvenza_animazione", -0.2f);
+        }
+    }
     void Start()
     {
         LeanTween.rotateAround(gameObject, transform.forward, -360f, 30f).setRepeat(-1);
@@ -60,9 +73,46 @@ public class InitialTriggerManager : MonoBehaviour
     IEnumerator EnableTabletAndTable()
     {
         yield return new WaitForSeconds(4.1f);
-        tavolo.SetActive(true);
+        var tabletModel= tablet.transform.GetChild(0).GetChild(0).GetChild(0);
+        
+
+        foreach (var renderer in tavoloMats)
+        {
+            LeanTween.value(1f, -0.2f, 4f).setOnUpdate((float value) =>
+            {
+                renderer.SetFloat("_Dissolvenza_animazione", value);
+            });
+        }
+        foreach (var renderer in matSpecials)
+        {
+            LeanTween.value( -0.2f, 1f, 4f).setOnUpdate((float value) =>
+            {
+                renderer.SetFloat("_Dissolvenza_animazione", value);
+            });
+        }
+        yield return new WaitForSeconds(4.5f);
+
+
+
+
         tablet.SetActive(true);
-        tablet.LeanMove(new Vector3(0f, 1.02189505f, 1.00300002f), 2f).setEaseInOutQuart();
-        tablet.LeanRotate(new Vector3(46f, 180f, 0), 3f).setEaseOutQuart();
+        tabletModel.GetChild(2).localScale = Vector3.zero;
+        tabletModel.GetChild(0).localScale = new Vector3(0, 0.3762262f, 0.3762262f);
+        tabletModel.GetChild(1).LeanMoveLocalZ(0, 1f).setOnComplete(() =>
+        {
+            tabletModel.GetChild(0).LeanScaleX(0.3762262f, 1).setOnComplete(() =>
+            {
+
+                tabletModel.GetChild(2).LeanScale(new Vector3(0.3762262f, 0.3762262f, 0.3762262f), 1).setOnComplete(() =>
+                {
+                    tablet.LeanMove(new Vector3(0f, 1.02189505f, 1.00300002f), 2f).setEaseInOutQuart();
+                    tablet.LeanRotate(new Vector3(46f, 180f, 0), 3f).setEaseOutQuart();
+                });
+            });
+        });
+       
+       
+       
+       
     }
 }
