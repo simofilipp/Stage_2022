5a494e86728561d5952e32b327c9f14693e86458
DavideManfredini <81241712+DavideManfredini@users.noreply.github.com>
2022-06-21 09:23:20 +0200

    Risolto problema trasparenze

--- Progetto_Stage/Assets/Simone/Scripts/Tablet.cs
+++ Progetto_Stage/Assets/Simone/Scripts/Tablet.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Tablet : MonoBehaviour
 {
+
+    [SerializeField]
+    GameObject immagineASchermo;
+
+
+    [SerializeField]
+    GameObject vetro;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vetro.GetComponent<MeshRenderer>().material.renderQueue = 3001;
+        immagineASchermo.GetComponent<MeshRenderer>().material.renderQueue = 3002;
     }
 
     // Update is called once per frame
