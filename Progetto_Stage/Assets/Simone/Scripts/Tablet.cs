a1c41a545be05a964946a484605c2a996f7d7020
DavideManfredini <81241712+DavideManfredini@users.noreply.github.com>
2022-06-21 13:06:10 +0200

    Miglioramnti_orbite

--- Progetto_Stage/Assets/Simone/Scripts/Tablet.cs
+++ Progetto_Stage/Assets/Simone/Scripts/Tablet.cs
@@ -14,8 +14,8 @@ public class Tablet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        vetro.GetComponent<MeshRenderer>().material.renderQueue = 3001;
-        immagineASchermo.GetComponent<MeshRenderer>().material.renderQueue = 3002;
+        //vetro.GetComponent<MeshRenderer>().material.renderQueue = 3001;
+        //immagineASchermo.GetComponent<MeshRenderer>().material.renderQueue = 3002;
     }
 
     // Update is called once per frame
