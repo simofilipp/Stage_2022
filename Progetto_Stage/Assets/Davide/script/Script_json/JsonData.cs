89581813fce3e37c4098ba071586aee6093de187
simofilipp <81241738+simofilipp@users.noreply.github.com>
2022-06-28 10:00:26 +0200

    Build nuova

--- Progetto_Stage/Assets/Davide/script/Script_json/JsonData.cs
+++ Progetto_Stage/Assets/Davide/script/Script_json/JsonData.cs
@@ -5,29 +5,22 @@ using UnityEngine;
 
 public class JsonData : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    //string filename = "WorldCities";
-    //string percorso;
+ 
     public TextAsset dataFile;
     public TextAsset dataGDP;
     public GameDatas gameData = new GameDatas();
     public GDPDatas gameDataGDP = new GDPDatas();
-    //public HealthDatas healthDatas = new HealthDatas();
 
     private void Awake()
     {
         ReadData();
-        AggiungiGDP();
-        WriteData();
-        Debug.Log(Application.dataPath);
+        //AggiungiGDP();
+        //WriteData();
+        //Debug.Log(Application.dataPath);
     
     }
     void Start()
     {
-        //percorso = Application.persistentDataPath + "/" + filename;
-        //Debug.Log(percorso);
-        
 
     }
 
@@ -39,13 +32,7 @@ public class JsonData : MonoBehaviour
     public void ReadData()
     {
         gameData = JsonUtility.FromJson<GameDatas>(dataFile.text);
-        gameDataGDP = JsonUtility.FromJson<GDPDatas>(dataGDP.text);
-        //healthDatas = JsonUtility.FromJson<HealthDatas>(dataHealth.text);
-
-        //foreach (var dato in healthDatas.dati)
-        //{
-        //    Debug.Log(dato.PrintAllData());
-        //}
+        //gameDataGDP = JsonUtility.FromJson<GDPDatas>(dataGDP.text);
 
     }
 
@@ -56,21 +43,21 @@ public class JsonData : MonoBehaviour
         File.WriteAllText(Application.dataPath + "/newJSON.json", output);
     }
 
-    void AggiungiGDP()
-    {
-        foreach(var dato in gameDataGDP.dati)
-        {
-            if(dato.Year == 2019)
-            {
-                foreach(var datoWC in gameData.dati)
-                {
-                    if(datoWC.capital=="primary" && datoWC.country == dato.Entity)
-                    {
-                        datoWC.gdp = dato.GDP;
-                        break;
-                    }
-                }
-            }
-        }
-    }
+    //void AggiungiGDP()
+    //{
+    //    foreach(var dato in gameDataGDP.dati)
+    //    {
+    //        if(dato.Year == 2019)
+    //        {
+    //            foreach(var datoWC in gameData.dati)
+    //            {
+    //                if(datoWC.capital=="primary" && datoWC.country == dato.Entity)
+    //                {
+    //                    datoWC.gdp = dato.GDP;
+    //                    break;
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 }
