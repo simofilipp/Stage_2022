3cfa1a8f250a5a10d09469262efafb59d11437ac
DavideManfredini <81241712+DavideManfredini@users.noreply.github.com>
2022-06-28 14:54:51 +0200

    Scena_backup_con_nuove_funzionlità

---
+++ Progetto_Stage/Assets/Davide/script/Pulsing_script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulsing_script : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.value(0f, .8f, 1f).setOnUpdate((float value) =>
+        {
+            this.gameObject.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", Color.white * value);
+
+        }).setLoopPingPong(-1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
