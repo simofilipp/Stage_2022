214f03f6cbc0b68372cb99d3d83a92d811a59e7a
DavideManfredini <81241712+DavideManfredini@users.noreply.github.com>
2022-07-18 14:05:23 +0200

    backup_18/07

---
+++ Progetto_Stage/Assets/Davide/script/Calcolo_gioco_latlong.cs
@@ -0,0 +1,21 @@
+using Oculus.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calcolo_gioco_latlong : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cursore;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
