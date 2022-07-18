a610e7043c09d76596a615a3a742a2850f3dd776
DavideManfredini <81241712+DavideManfredini@users.noreply.github.com>
2022-07-18 14:06:00 +0200

    backup_salvataggio

--- Progetto_Stage/Assets/Davide/script/Calcolo_gioco_latlong.cs
+++ Progetto_Stage/Assets/Davide/script/Calcolo_gioco_latlong.cs
@@ -1,4 +1,5 @@
 using Oculus.Interaction;
+using Oculus.Interaction.Surfaces;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,20 @@ public class Calcolo_gioco_latlong : MonoBehaviour
 {
     [SerializeField]
     GameObject cursore;
+
+    IPointableSurface pointableSurface;
     // Start is called before the first frame update
     void Start()
     {
-       
+      
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+    public void RilevaPinch() 
     {
         
     }
