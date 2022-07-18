5dde8cbdd28b6fbfb1c50654155b5fafbef30e26
simofilipp <81241738+simofilipp@users.noreply.github.com>
2022-07-18 18:00:00 +0200

    Prova_long_lat_gioco

--- Progetto_Stage/Assets/Davide/script/Calcolo_gioco_latlong.cs
+++ Progetto_Stage/Assets/Davide/script/Calcolo_gioco_latlong.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 
 public class Calcolo_gioco_latlong : MonoBehaviour
 {
-    [SerializeField]
-    GameObject cursore;
+    [SerializeField] GameObject cursore;
+    [SerializeField] GameObject terra;
+    [SerializeField] GameObject puntoPrefab;
+    [SerializeField] Transform coordinateZero;
 
-    IPointableSurface pointableSurface;
+    float lat,lng;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,23 @@ public class Calcolo_gioco_latlong : MonoBehaviour
     }
     public void RilevaPinch() 
     {
-        
+        if (cursore.activeSelf)
+        {
+            var puntoIstanziato= Instantiate(puntoPrefab, cursore.transform.position, coordinateZero.rotation);
+            puntoIstanziato.transform.parent = terra.transform;
+            float proiezioneRaggio = Mathf.Sqrt(((puntoIstanziato.transform.position.x - terra.transform.position.x) * (puntoIstanziato.transform.position.x - terra.transform.position.x))
+                + ((puntoIstanziato.transform.position.z - terra.transform.position.z) * (puntoIstanziato.transform.position.z - terra.transform.position.z)));
+            float raggio = Mathf.Sqrt((proiezioneRaggio * proiezioneRaggio) + (puntoIstanziato.transform.position.y * puntoIstanziato.transform.position.y));
+
+            lng = Mathf.Acos((((coordinateZero.position.z- puntoIstanziato.transform.position.z)* (coordinateZero.position.z - puntoIstanziato.transform.position.z))+ ((coordinateZero.position.x - puntoIstanziato.transform.position.x) * (coordinateZero.position.x - puntoIstanziato.transform.position.x))
+                - (raggio * raggio) - ((puntoIstanziato.transform.position.x* puntoIstanziato.transform.position.x) + (puntoIstanziato.transform.position.z* puntoIstanziato.transform.position.z))) / (-2 * raggio) * Mathf.Sqrt((puntoIstanziato.transform.position.x * puntoIstanziato.transform.position.x) + (puntoIstanziato.transform.position.z * puntoIstanziato.transform.position.z)))*Mathf.Rad2Deg;
+
+
+
+
+            //lng = Mathf.Asin((coordinateZero.position.z/Mathf.Sqrt((coordinateZero.position.z* coordinateZero.position.z)+(coordinateZero.position.x * coordinateZero.position.x))))*Mathf.Rad2Deg + Mathf.Asin((puntoIstanziato.transform.position.z / Mathf.Sqrt((puntoIstanziato.transform.position.z * puntoIstanziato.transform.position.z) + (puntoIstanziato.transform.position.x * puntoIstanziato.transform.position.x))))*Mathf.Rad2Deg;
+            lat = Mathf.Asin((coordinateZero.position.y) / raggio) * Mathf.Rad2Deg + Mathf.Asin((puntoIstanziato.transform.position.y) / raggio)*Mathf.Rad2Deg;
+            Debug.LogWarning("lat: "+lat+"\nlong: "+lng);
+        }
     }
 }
